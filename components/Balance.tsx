dbadbf5a1e870bd91c0f9665ac9123404ec61900
guptariya01 <guptariya210307@gmail.com>
2025-07-15 12:05:35 +0530

    new Verifying kindof commit

--- components/Balance.tsx
+++ components/Balance.tsx
@@ -1,12 +1,12 @@
-import getUserBalance from '@/app/actions/getUserBalance';
-import { addCommas } from '@/lib/utils';
+import getUserBalance from "@/app/actions/getUserBalance";
+import { addCommas } from "@/lib/utils";
 
 const Balance = async () => {
   const { balance } = await getUserBalance();
 
   return (
     <>
-      <h4>Your Balance</h4>
+      <h2>Your Balance</h2>
       <h1>${addCommas(Number(balance?.toFixed(2) ?? 0))}</h1>
     </>
   );
